19c40c54231ad7eacefd1fdb5930e452c64f6e24
13jk092koizumi <13jk092@ms.dendai.ac.jp>
2016-05-31 17:51:21 +0900

    Wi-Fi get signal 2016/5/31

---
+++ MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Net.Wifi;
+using Android.OS;
+
+namespace GetWifi {
+    [Activity(Label = "GetWifi", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity {
+
+        protected override void OnCreate(Bundle bundle) {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            var linearLayout = FindViewById<LinearLayout>(Resource.Id.LLayout);
+            
+            //TextView生成
+            var tv = new TextView(this);
+
+            //wifi情報を取得
+            var wifi = (WifiManager)GetSystemService(WifiService);
+            //アクセスポイントのスキャン
+            wifi.StartScan();
+            //結果の取得
+            var results = wifi.ScanResults;
+            //結果をTextViewにセット
+            var sb = new System.Text.StringBuilder();
+
+            for (int i=0; i<results.Count; ++i) {
+                sb.Append(results[i].ToString());
+                sb.Append("\n");                
+            }
+            tv.SetTextSize(Android.Util.ComplexUnitType.Dip, 20);
+            tv.Text = sb.ToString();
+            linearLayout.AddView(tv);
+            
+        }
+    }
+}
+
