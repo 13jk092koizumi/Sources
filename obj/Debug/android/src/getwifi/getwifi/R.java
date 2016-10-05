83d0a5c1c72f1392a4dde6290e1790a0b1d32d5d
13jk092koizumi <13jk092@ms.dendai.ac.jp>
2016-10-05 23:41:50 +0900

    レイアウトファイルの変更

--- obj/Debug/android/src/getwifi/getwifi/R.java
+++ obj/Debug/android/src/getwifi/getwifi/R.java
@@ -178,9 +178,9 @@ public final class R {
         public static final int LLayout=0x7f070003;
         public static final int ThreadProcess=0x7f070002;
         public static final int btnAddData=0x7f070008;
-        public static final int btnDelete=0x7f07000a;
+        public static final int btnDelete=0x7f070009;
         public static final int btnInput=0x7f070006;
-        public static final int btnShowTable=0x7f070009;
+        public static final int btnShowTable=0x7f07000a;
         public static final int button1=0x7f070000;
         public static final int button2=0x7f070001;
         public static final int inputTxtLL=0x7f070004;
@@ -193,11 +193,11 @@ public final class R {
         public static final int rowtext4=0x7f070012;
         public static final int rowtext5=0x7f070016;
         public static final int rowtext6=0x7f070017;
-        public static final int rowtext7=0x7f070018;
+        public static final int rowtext7=0x7f070019;
         public static final int tableLayout=0x7f07000d;
         public static final int tableRow=0x7f07000e;
         public static final int tb_TextView1=0x7f070014;
-        public static final int tb_btnBack=0x7f070019;
+        public static final int tb_btnBack=0x7f070018;
         public static final int tb_tableLayout=0x7f070015;
         public static final int textView1=0x7f07000c;
     }
